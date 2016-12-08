eeb165cd92ef1316b5be434aec518deb85eace3c
puttaa <alwal.putta@roche.com>
2016-12-08 15:56:04 -0800

    added and modified a file

--- cat_files.py
+++ cat_files.py
@@ -8,6 +8,8 @@ for files in os.walk('.git'):
 			for f in ff:
 #				print (f)
 				print (os.path.abspath(f))
+				full_file_name =  (os.path.abspath(f))
+				print (os.system("cat $full_file_path"))
 
 #('..', ['Docker', 'Python', 'Unix'], [])
 #('../Docker', ['mydockerbuild', 'nginx'], ['index.html'])
